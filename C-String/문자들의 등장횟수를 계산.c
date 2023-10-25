d50f7663e788a4ec3e0877c432c87621a2a8e101
Yeong-Jin Kim <46725938+kzz22e@users.noreply.github.com>
2023-10-25 10:51:09 +0900

    Update 문자들의 등장횟수를 계산.c

--- C-String/문자들의 등장횟수를 계산.c
+++ C-String/문자들의 등장횟수를 계산.c
@@ -12,7 +12,7 @@
 int main(void) {
 
     //입력받은 문자열을 저장하는 코드
-    char save[SIZE] = { 0 };
+    char saveText[SIZE] = { 0 };
 
     //아스키 코드의 범위만큼 범위지정 0~127
     int freq[ASCII] = { 0 };
@@ -21,14 +21,14 @@ int main(void) {
     printf("텍스트를 입력하시오: ");
     //scanf()는 공백이 있으면 공백 앞자리까지 저장하기 때문에
     //"%[^n]s"를 이용해 줄바꿈을 하기 전까지 모든 문자를 입력받늗다
-    scanf("%[^\n]s", s);
+    scanf("%[^\n]s", saveText);
 
-    //입력받은 문자열의 크기만큼 반복 : strlen(s) 함수는 문자열 s의 길이를 반환합니다.
-    //freq[s[i]]++; : 입력된 문자열에서 문자 's[i]'의 빈도수를 증가시킵니다.즉, 
+    //입력받은 문자열의 크기만큼 반복 : strlen(saveText) 함수는 문자열 s의 길이를 반환합니다.
+    //freq[saveText[i]]++; : 입력된 문자열에서 문자 'saveText[i]'의 빈도수를 증가시킵니다.즉, 
     //해당 문자가 나타날 때마다 freq 배열에서 해당 문자의 ASCII 코드 위치에 있는 요소를 1씩 증가시킵니다.
-    for (int i = 0; i < strlen(s); i++) {
-        //예:s[i] = 'k' 라고 가정하면 freq[K]++; -> freq[107]++ 
-        freq[s[i]]++;
+    for (int i = 0; i < strlen(saveText); i++) {
+        //예:saveText[i] = 'k' 라고 가정하면 freq[K]++; -> freq[107]++ 
+        freq[saveText[i]]++;
     }
     //if (freq[j] != 0): 문자열에 해당 문자가 하나 이상 나타났다면 아래의 코드를 실행
     for (int j = 'a'; j <= 'z'; j++) {
