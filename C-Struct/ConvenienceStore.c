d223a1124a8ab4f9a664d0e0dbf4d6f55de81861
Yeong-Jin Kim <46725938+kzz22e@users.noreply.github.com>
2023-11-15 12:39:51 +0900

    Update ConvenienceStore.c

--- C-Struct/ConvenienceStore.c
+++ C-Struct/ConvenienceStore.c
@@ -1,47 +1,46 @@
-#pragma warning(disable:4996)  // 특정 경고 메시지를 억제
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
-//기호상수 선언
 #define NAME_SIZE 30
 #define ITEM_SIZE 10
 #define ITEM_LIST 10
 
-//구조체 Inventory_Management 선언
 struct Inventory_Management {
     char ITEM_name[NAME_SIZE];
     unsigned long ITEM_price;
     unsigned int ITEM_quantity;
 };
 
-int main(){
-    
-    //Y_STORE이라는 구조체 배열 선언
-    struct Inventory_Management Y_STORE[ITEM_LIST];
-    struct Inventory_Management* p = &Y_STORE;
-    
-    Save_Inventory_Management(&Y_STORE);
-    
-    
-}
+void Save_Inventory_Management(struct Inventory_Management Y_STORE[]) {
+    for (int i = 0; i < ITEM_SIZE; i++) {
+        printf("제품명을 입력하세요: ");
+        scanf("%29[^\n]", Y_STORE[i].ITEM_name);
 
-void Save_Inventory_Management(struct Inventory_Management* Y_STORE[]){
-    
-    for(int i = 0;i<ITEM_SIZE;I++){
-        printf("상품명을 입력하세요 :");
-        scanf("%s[^\n]s", *Y_STORE[i].ITEM_name);
-    
-        printf("");
-        scanf("%s[^\n]ld", *Y_STORE[i].ITEM_price);
-    
-        printf("");
-        scanf("%s[^\n]d", *Y_STORE[i].ITEM_quantity);
+        printf("제품 가격을 입력하세요: ");
+        scanf("%lu", &Y_STORE[i].ITEM_price);
+
+        printf("제품 수량을 입력하세요: ");
+        scanf("%u", &Y_STORE[i].ITEM_quantity);
     }
 }
 
+void Print_Inventory_Management(const struct Inventory_Management Y_STORE[]) {
+    for (int i = 0; i < ITEM_SIZE; i++) {
+        printf("제품명: %s\n", Y_STORE[i].ITEM_name);
+        printf("제품 가격: %lu\n", Y_STORE[i].ITEM_price);
+        printf("제품 수량: %u\n\n", Y_STORE[i].ITEM_quantity);
+    }
+}
 
+int main() {
+    struct Inventory_Management Y_STORE[ITEM_LIST];
 
+    // Save_Inventory_Management 함수를 호출하여 데이터를 입력합니다.
+    Save_Inventory_Management(Y_STORE);
 
+    // 입력한 내용을 출력합니다.
+    Print_Inventory_Management(Y_STORE);
 
-
+    return 0;
+}
