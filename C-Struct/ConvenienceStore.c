04b27ad7f5303e418ffdc2d2db39477ebcbc7d56
Yeong-Jin Kim <46725938+kzz22e@users.noreply.github.com>
2023-11-15 11:10:32 +0900

    Update ConvenienceStore.c

--- C-Struct/ConvenienceStore.c
+++ C-Struct/ConvenienceStore.c
@@ -2,3 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+
+//기호상수 선언
+#define NAME_SIZE 30
+#define ITEM_SIZE 10
+#define QUANTITY 3000
+
+//구조체 재고관리 선언
+struct Inventory_Management {
+    char ITEM_name[NAME_SIZE];
+    unsigned long ITEM_price;
+    unsigned int ITEM_QUANTITY;
+};
