b514d61f29793b9d0d8bd909107cb25c8cd847cd
Yeong-Jin Kim <46725938+kzz22e@users.noreply.github.com>
2023-11-15 12:34:03 +0900

    Update ConvenienceStore.c

--- C-Struct/ConvenienceStore.c
+++ C-Struct/ConvenienceStore.c
@@ -6,14 +6,13 @@
 //기호상수 선언
 #define NAME_SIZE 30
 #define ITEM_SIZE 10
-#define QUANTITY 3000
 #define ITEM_LIST 10
 
 //구조체 Inventory_Management 선언
 struct Inventory_Management {
     char ITEM_name[NAME_SIZE];
     unsigned long ITEM_price;
-    unsigned int ITEM_QUANTITY;
+    unsigned int ITEM_quantity;
 };
 
 int main(){
@@ -28,14 +27,17 @@ int main(){
 }
 
 void Save_Inventory_Management(struct Inventory_Management* Y_STORE[]){
-    printf("");
-    scanf("");
     
-    printf("");
-    scanf("");
+    for(int i = 0;i<ITEM_SIZE;I++){
+        printf("상품명을 입력하세요 :");
+        scanf("%s[^\n]s", *Y_STORE[i].ITEM_name);
     
-    printf("");
-    scanf("");
+        printf("");
+        scanf("%s[^\n]ld", *Y_STORE[i].ITEM_price);
+    
+        printf("");
+        scanf("%s[^\n]d", *Y_STORE[i].ITEM_quantity);
+    }
 }
 
 
