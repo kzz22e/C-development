08ff6e4adb1b52a9ff42c36017426c4bd9a4aaea
Yeong-Jin Kim <46725938+kzz22e@users.noreply.github.com>
2023-11-16 05:52:38 +0900

    Update ConvenienceStore.c

--- C-Struct/ConvenienceStore.c
+++ C-Struct/ConvenienceStore.c
@@ -9,8 +9,20 @@
 #define ITEM_SIZE 10
 #define ITEM_LIST 10
 
+//메뉴 상수선언
+#define FIRST 49    //1
+#define SECOND 50    //2
+#define THIRD 51    //3
+#define FOURTH 52    //4
+
 //================================================================================
+// 매뉴를 함수로 표현
+void Main_Menu();        //메인메뉴
+void Inventory_Status(struct Inventory_Management Y_STORE[]); //1.재고현황
+
+//텍스트를 간단하게 출력하는 함수
 void Text_Print_list(char Text_list[]);
+void Print_Inventory_Management(struct Inventory_Management Y_STORE[]);
 
 //검사하는 함수
 int Check_ITEM_name(char* str);
@@ -19,10 +31,6 @@ int Check_ITEM_name(char* str);
 void SAVE_ITEM_name(char* question, char* Input_value, int (*function)(char*), char* errorMessage);
 void SAVE_ITEM_priceORquantity(char* question, long* Input_value, char* errorMessage);
 void Save_Inventory_Management(struct Inventory_Management Y_STORE[]);
-
-//입력받은값 출력확인
-void Print_Inventory_Management(const struct Inventory_Management Y_STORE[]);
-
 //================================================================================
 
 //구조체 선언
@@ -39,13 +47,52 @@ int main() {
     // Save_Inventory_Management 함수를 호출하여 데이터를 입력합니다.
     Save_Inventory_Management(Y_STORE);
 
-    // 입력한 내용을 출력합니다.
-    Print_Inventory_Management(Y_STORE);
+    Main_Menu(Y_STORE);
 
     return 0;
 }
 
-//텍스트 출력 저장
+void Main_Menu(struct Inventory_Management Y_STORE[]) {
+    short menu = 0,sub_menu = 0;
+    while(1){
+        while(1){
+            Text_Print_list("메인메뉴");
+            menu = _getch();
+                if ((menu >= FIRST && menu <= FOURTH)) {
+                    break;
+                }
+                Text_Print_list("없는 메뉴입니다.");
+        }
+        if (menu == FIRST) {
+            Inventory_Status(Y_STORE);
+        }
+    }
+}
+//재고 현황 메뉴
+void Inventory_Status(struct Inventory_Management Y_STORE[]){
+
+    int i;
+    short sub_menu = 0;
+    
+    while (1) {
+        Text_Print_list("재고 현황");
+        sub_menu = _getch();
+        if ((sub_menu >= FIRST && sub_menu <= THIRD)) {
+            break;
+        }
+    }
+    if (sub_menu = FIRST) {
+
+    }
+    else if (sub_menu = SECOND) {
+
+    }
+    else if (sub_menu = THIRD) {
+
+    }   
+}
+
+//텍스트 출력 리스트 저장
 void Text_Print_list(char Text_list[]) {
 
     //안내선 출력
@@ -53,9 +100,30 @@ void Text_Print_list(char Text_list[]) {
         printf("____________________________________________\n\n");
         return;
     }
-
+    //메인메뉴 출력
+    if (strcmp(Text_list, "메인메뉴") == 0) {
+        printf("____________________________________________\n");
+        printf("[메인메뉴]\n");
+        printf("1. 재고 현황\n");
+        printf("2. 재고 검색\n");
+        printf("3. 재고 관리\n");
+        printf("4. 종료\n");
+        printf("____________________________________________\n");
+        return;
+    }
+    //1. 재고 현황 메뉴 출력
+    if (strcmp(Text_list, "재고 현황") == 0) {
+        printf("____________________________________________\n");
+        printf("[정렬방식 선택]\n");
+        printf("1. 제품 알파벳순 정렬\n");
+        printf("2. 제품 가격순 정렬\n");
+        printf("3. 제품 수량순 정렬\n");
+        printf("____________________________________________\n");
+        return;
+    }
     //경고 문자 출력
     printf("\n\n\n\n\n____________________경고____________________\n");
+
     if (strcmp(Text_list, "영어, 숫자만 입력 가능합니다.") == 0) {
         printf("\n\n영어, 숫자만 입력 가능합니다.\n");
         printf("다시 입력해 주세요.\n");
@@ -63,8 +131,12 @@ void Text_Print_list(char Text_list[]) {
     else if (strcmp(Text_list, "양의 숫자를 입력하세요.") == 0) {
         printf("\n\n양의 숫자를 입력하세요.\n");
     }
+    else if (strcmp(Text_list, "없는 메뉴입니다.") == 0) {
+        printf("\n\n없는 메뉴입니다. 다시입력해주세요\n");
+    }
     printf("\n____________________________________________\n\n\n\n\n\n");
 
+    
 }
 
 //영어 알파벳, 숫자 여부를 확인하는 함수
@@ -135,9 +207,9 @@ void Save_Inventory_Management(struct Inventory_Management Y_STORE[]) {
 void Print_Inventory_Management(struct Inventory_Management Y_STORE[]) {
     int i;
     for (i = 0; i < ITEM_SIZE; i++) {
+        printf("[%d번 제품]\n", i);
         printf("제품명: %s\n", Y_STORE[i].ITEM_name);
         printf("제품 가격: %lu\n", Y_STORE[i].ITEM_price);
         printf("제품 수량: %u\n\n", Y_STORE[i].ITEM_quantity);
     }
 }
-
