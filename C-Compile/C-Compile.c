48fb90a1a69b2e010552dd5e7b560e99f5b51cec
영진 김 <a38027928@gmail.com>
2023-11-18 20:16:31 +0900

    123

--- C-Compile/C-Compile.c
+++ C-Compile/C-Compile.c
@@ -1,58 +1,275 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
-#pragma warning(disable:4996)
 #include <stdio.h>
-#include <stdlib.h>		// RAND_MAX
-#include <conio.h>
-#include <Windows.h>	//srand(GetTickCount());
-#include <time.h>		//srand(time(NULL));
-#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-//선언
-long long Save_integer();
-long long exponential(long long);
+//상수 선언
+#define NAME_SIZE 30
+#define ITEM_SIZE 10
+#define ITEM_LIST 10
 
-//전역 변수
-long long int integer = 0;
-int savePM = 0; //1은 양수 0은 음수
+//메뉴 번호 상수선언
+#define FIRST 49        //1
+#define SECOND 50       //2
+#define THIRD 51        //3
+#define FOURTH 52       //4
 
-//실행
-int main(void) {
-	long long base = 0; 
-	while (1) {
-		base = exponential(Save_integer());
-		if (savePM == 0){
-			base = -base;
-			printf("%lld\n", base);
-		}
-		else if (savePM == 1) {
-			printf("%lld\n", base);
-		}
-	}
-	
-	return 0;
+//================================================================================
+// 매뉴를 함수로 표현
+void Main_Menu();        //메인메뉴
+int Inventory_Status(struct Inventory_Management Y_STORE[]); //1.재고현황 메뉴
+
+//텍스트를 간단하게 출력하는 함수
+void Print_Simple(char Text_list[]);    //간단하게 사용가능한 텍스트
+void Print_Errer(char Text_list[]);     //에러 텍스트 모음
+void Print_Menu(char Text_list[]);      // 메뉴 텍스트 모음
+void Print_Inventory_Management(struct Inventory_Management Y_STORE[]); //재고 출력
+
+//검사하는 함수
+int Check_ITEM_name(char* str);                             //제품이름을 검사하는 함수
+int compareStructs(const void* a, const void* b);           //구조체에서 ITEM_name을 비교하는 함수
+int compareStructsByPrice(const void* a, const void* b);    //구조체에서 ITEM_price을 비교하는 함수
+
+//저장하는 함수
+void SAVE_ITEM_name(char* question, char* Input_value, int (*function)(char*), char* errorMessage);
+void SAVE_ITEM_priceORquantity(char* question, long* Input_value, char* errorMessage);
+void Save_Inventory_Management(struct Inventory_Management Y_STORE[]);
+//================================================================================
+
+//구조체 선언
+typedef struct Inventory_Management {
+    char ITEM_name[NAME_SIZE];
+    long ITEM_price;
+    long ITEM_quantity;
+}Inventory_Management;
+
+//구조체에서 ITEM_name을 비교하는 함수
+int compareStructs_name(const void* a, const void* b) {
+    // a와 b를 각각 구조체 포인터로 캐스팅한 후, ITEM_name를 비교하여 반환
+    //(struct Inventory_Management*) 구조체 포인터로 형변환
+    return strcmp(((struct Inventory_Management*)a)->ITEM_name, ((struct Inventory_Management*)b)->ITEM_name);
+}
+//구조체에서 ITEM_price을 비교하는 함수
+int compareStructs_price(const void* a, const void* b) {
+    // a와 b를 각각 구조체 포인터로 캐스팅한 후, ITEM_price를 비교하여 반환
+    //(struct Inventory_Management*) 구조체 포인터로 형변환
+    return ((struct Inventory_Management*)a)->ITEM_price - ((struct Inventory_Management*)b)->ITEM_price;
+}
+//구조체에서 ITEM_quantity을 비교하는 함수
+int compareStructs_quantity(const void* a, const void* b) {
+    // a와 b를 각각 구조체 포인터로 캐스팅한 후, ITEM_quantity를 비교하여 반환
+    //(struct Inventory_Management*) 구조체 포인터로 형변환
+    return ((struct Inventory_Management*)a)->ITEM_quantity - ((struct Inventory_Management*)b)->ITEM_quantity;
 }
 
-//정수를 입력받는 함수
-long long Save_integer() {
 
-	printf("정수를 입력하시오 : ");
-	scanf("%lld", &integer);
+//프로그램 시작
+int main() {
+    Inventory_Management Y_STORE[ITEM_LIST] = {
+        {"apple", 12345, 13},
+        {"orange", 67890, 8},
+        {"banana", 54321, 20},
+        {"grape", 98765, 15},
+        {"watermelon", 24680, 5},
+        {"strawberry", 13579, 25},
+        {"kiwi", 11223, 18},
+        {"pineapple", 33445, 10},
+        {"mango", 55667, 22},
+        {"pear", 77889, 17}
+    };
 
-	//음수라면 일단 savePM 음수라고 저장하고 양수로 바꿔준다
-	if (integer < 0) {
-		savePM = 0;			//음수라고 저장
-		return -integer;	//양수로 변환후 반환
-	}
-	//양수라면 일단 savePM 양수라고 저장
-		savePM = 1;			//양수라고 저장
-		return integer;		//양수값 반환
+    //Save_Inventory_Management(Y_STORE);
+
+    Main_Menu(Y_STORE);
+
+    return 0;
 }
 
-//입력받은 정수를 세제곱해서 내림차순으로 더하는 재귀 함수
-long long exponential(long long integer) {
-	if (integer == 1) {
-		return 1;
-	}
-	return pow(integer, 3) + exponential(integer - 1);
+//메인메뉴
+void Main_Menu(struct Inventory_Management Y_STORE[]) {
+    short menu = 0,sub_menu = 0;
+    while(1){
+        while(1){
+            Print_Menu("메인 메뉴");
+            menu = _getch();
+                if ((menu >= FIRST && menu <= FOURTH)) {
+                    break;
+                }
+                Print_Errer("없는 메뉴입니다.");
+        }
+        //1. 재고 현황
+        while(1){
+            if (menu == FIRST) {
+                if (Inventory_Status(Y_STORE) == 4) { break; }
+            }
+        }
+    }
 }
 
+//재고 현황 메뉴
+int Inventory_Status(struct Inventory_Management Y_STORE[]){
+
+    int i = 0;
+    short sub_menu = 0;
+    
+    while (1) {
+        Print_Menu("재고 현황");
+        sub_menu = _getch();
+        if (sub_menu >= FIRST && sub_menu <= FOURTH) {
+            break;
+        }
+        else {
+            Print_Errer("없는 메뉴입니다.");
+        }
+    }
+    if (sub_menu == FIRST) {
+        //qsort함수는 오름차순 정렬기준
+        qsort(Y_STORE, ITEM_LIST, sizeof(struct Inventory_Management), compareStructs_name);
+        Print_Inventory_Management(Y_STORE);
+        return 1;
+    }
+    else if (sub_menu == SECOND) {
+        //qsort함수는 오름차순 정렬기준
+        qsort(Y_STORE, ITEM_LIST, sizeof(struct Inventory_Management), compareStructs_price);
+        Print_Inventory_Management(Y_STORE);
+        return 2;
+    }
+    else if (sub_menu == THIRD) {
+        qsort(Y_STORE, ITEM_LIST, sizeof(struct Inventory_Management), compareStructs_quantity);
+        Print_Inventory_Management(Y_STORE);
+        return 3;
+    }
+    else if (sub_menu == FOURTH) {
+        return 4;
+    }
+}
+
+
+//메뉴 출력 함수
+void Print_Menu(char Text_list[]) {
+    
+    //메인메뉴
+    if (strcmp(Text_list, "메인 메뉴") == 0) {
+        printf("____________________________________________\n");
+        printf("[메인 메뉴]\n");
+        printf("1. 재고 현황\n");
+        printf("2. 재고 검색\n");
+        printf("3. 재고 관리\n");
+        printf("4. 종료\n");
+        printf("____________________________________________\n");
+        return;
+    }
+    //1. 재고 현황 메뉴
+    else if (strcmp(Text_list, "재고 현황") == 0) {
+        printf("____________________________________________\n");
+        printf("[정렬방식 선택]\n");
+        printf("1. 제품 알파벳순 정렬\n");
+        printf("2. 제품 가격순 정렬\n");
+        printf("3. 제품 수량순 정렬\n");
+        printf("4. 메인 메뉴\n");
+        printf("____________________________________________\n");
+        return;
+    }
+}
+//에러메시지 출력 함수
+void Print_Errer(char Text_list[]) {
+    printf("\n\n\n\n\n____________________경고____________________\n");
+
+    if (strcmp(Text_list, "영어, 숫자만 입력 가능합니다.") == 0) {
+        printf("\n\n영어, 숫자만 입력 가능합니다.\n");
+        printf("다시 입력해 주세요.\n");
+    }
+    else if (strcmp(Text_list, "양의 숫자를 입력하세요.") == 0) {
+        printf("\n\n양의 숫자를 입력하세요.\n");
+    }
+    else if (strcmp(Text_list, "없는 메뉴입니다.") == 0) {
+        printf("\n\n없는 메뉴입니다. 다시입력해주세요\n");
+    }
+    printf("\n____________________________________________\n\n\n\n\n\n");
+}
+//간단텍스트 출력 함수
+void Print_Simple(char Text_list[]) {
+    if (strcmp(Text_list, "안내선") == 0) {
+        printf("____________________________________________\n\n");
+        return;
+    }
+}
+
+
+
+
+//영어 알파벳, 숫자 여부를 확인하는 함수
+int Check_ITEM_name(char* str) {
+
+    // 문자열 처음부터 널문자까지 반복해서 확인한다.
+    while (*str != '\0') {
+
+        // 현재 문자가 알파벳 또는 숫자가 아니면서 개행 문자도 아닌 경우
+        if (!isalnum(*str) && *str != '\n' && *str != '\r') {
+            return 0; // 특수문자가 있으면 0 반환
+        }
+        str++;
+    }
+    return 1; // 특수문자가 없으면 1 반환
+}
+
+//매개변수에 함수의 포인터를 추가해 보다 함수를 이용해 함수를 사용할 수 있게 했습니다
+//값을 입력받은 뒤 함수로 검사한 뒤 저장한다
+void SAVE_ITEM_name(char* question, char* Input_value, int (*function)(char*), char* errorMessage) {
+    while (1) {
+
+        printf("%s", question);
+        scanf("%29[^\n]s", Input_value);
+        getchar(); // Enter 키 제거
+
+        if (function(Input_value) == 1) {
+            break;
+        }
+
+        Print_Errer(errorMessage);
+    }
+}
+
+// 입력받고 유효한지 확인까지 수행하는 함수
+void SAVE_ITEM_priceORquantity(char* question, long* Input_value, char* errorMessage) {
+    while (1) {
+        printf("%s", question);
+        if (scanf("%ld", Input_value) == 1 && *Input_value > 0) {
+            getchar(); // Enter 키 제거
+            break;
+        }
+        Print_Errer(errorMessage);
+        getchar(); // 입력 버퍼 비우기
+    }
+}
+
+//재고를 저장하는 함수
+void Save_Inventory_Management(struct Inventory_Management Y_STORE[]) {
+    for (int i = 0; i < ITEM_SIZE; i++) {
+
+        Print_Simple("안내선");
+
+        // 입력받기 함수 호출
+        SAVE_ITEM_name("제품명을 입력하세요: ", Y_STORE[i].ITEM_name, Check_ITEM_name, "영어, 숫자만 입력 가능합니다.");
+
+        // 가격 입력받기
+        SAVE_ITEM_priceORquantity("제품 가격을 입력하세요: ", &Y_STORE[i].ITEM_price, "양의 숫자를 입력하세요.");
+
+        // 수량 입력받기
+        SAVE_ITEM_priceORquantity("제품 수량을 입력하세요: ", &Y_STORE[i].ITEM_quantity, "양의 숫자를 입력하세요.");
+
+        Print_Simple("안내선");
+    }
+}
+
+//재고를 출력하는 함수
+void Print_Inventory_Management(struct Inventory_Management Y_STORE[]) {
+    int i;
+    for (i = 0; i < ITEM_SIZE; i++) {
+        printf("[%d번 제품]\n", i);
+        printf("제품명: %s\n", Y_STORE[i].ITEM_name);
+        printf("제품 가격: %lu\n", Y_STORE[i].ITEM_price);
+        printf("제품 수량: %u\n\n", Y_STORE[i].ITEM_quantity);
+    }
+}
--- C-Struct/ConvenienceStore.c
+++ C-Struct/ConvenienceStore.c
@@ -1,27 +1,61 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <wchar.h>
 
+//상수 선언
 #define NAME_SIZE 30
 #define ITEM_SIZE 10
 #define ITEM_LIST 10
 
+//구조체 선언
 struct Inventory_Management {
     char ITEM_name[NAME_SIZE];
     unsigned long ITEM_price;
     unsigned int ITEM_quantity;
 };
+
 //재고를 저장하는 함수
 void Save_Inventory_Management(struct Inventory_Management Y_STORE[]) {
+    char Text_Print[100] = { 0 };
     for (int i = 0; i < ITEM_SIZE; i++) {
-        printf("제품명을 입력하세요: ");
-        scanf("%29[^\n]", Y_STORE[i].ITEM_name);
-        
-        printf("제품 가격을 입력하세요: ");
-        scanf("%lu", &Y_STORE[i].ITEM_price);
 
-        printf("제품 수량을 입력하세요: ");
-        scanf("%u", &Y_STORE[i].ITEM_quantity);
+        while(1){
+            printf("제품명을 입력하세요: ");
+            scanf("%29[^\n]", Y_STORE[i].ITEM_name);
+                if (Check_ITEM_name_UTF8(Y_STORE[i].ITEM_name)==1) {
+                    break;
+                }
+                strcpy(Text_Print, "한글, 영어, 숫자만 가능");
+                Text_Print_list(Text_Print);
+
+        }
+        while (1) {
+            printf("제품 가격을 입력하세요: ");
+            scanf("%lu", &Y_STORE[i].ITEM_price);
+                if (Y_STORE[i].ITEM_price > 0) {
+                    break;
+                }
+                
+        }
+
+        while (1) {
+            printf("제품 수량을 입력하세요: ");
+            scanf("%u", &Y_STORE[i].ITEM_quantity);
+        }
+
+    }
+}
+//텍스트 출력 저장
+void Text_Print_list(const char *Text_list) {
+    if (strcmp(*Text_list, "한글, 영어, 숫자만 가능")) {
+        printf("===============주의===============\n");
+        printf("한글, 영어, 숫자만 입력가능합니다.\n");
+        printf("다시 입력해 주세요.\n");
+        printf("===============주의===============\n");
+    }
+    else if{
+
     }
 }
 
@@ -33,8 +67,41 @@ void Print_Inventory_Management(const struct Inventory_Management Y_STORE[]) {
         printf("제품 수량: %u\n\n", Y_STORE[i].ITEM_quantity);
     }
 }
-//입력받은 값이 한글인지 확인
-int Check_ITEM_name(){
+
+// UTF-8 문자열에서 한글, 영어 알파벳, 숫자 여부를 확인하는 함수
+int Check_ITEM_name_UTF8(const char* str) {
+
+    // 문자열 처음부터 널문자까지 반복해서 확인한다.
+    while (*str != '\0') {
+        // 와이드 문자를 저장하기 위한 변수를 선언하는 부분
+        wchar_t wchar;
+        // mbtowc 함수는 멀티바이트 문자열에서 와이드 문자로 변환하는 함수입니다.
+        // 즉 멀티바이트 문자열(str)을 와이드 문자로 (wchar)에 변환하겠다는 의미입니다.
+        // 함수 형식 : int mbtowc(wchar_t *pwc, const char *s, size_t n);
+        // wchar_t *pwc : 변환된 와이드 문자가 저장될 포인터입니다.
+        // const char *s : 변환할 멀티바이트 문자열의 포인터입니다.
+        // size_t n : 변환할 최대 바이트 수입니다.
+        int result = mbtowc(&wchar, str, MB_CUR_MAX);
+
+        // mbtowc 함수는 다중 바이트 문자열을 와이드 문자로 변환하는데, 이때 변환 중에 문제가 발생하면 반환값으로 -1을 돌려줍니다.
+        // 이 코드에서 result == -1은 mbtowc 함수의 변환에 실패했다는 것을 의미합니다.
+        // 이는 주로 잘못된 UTF-8 형식의 문자열이 들어왔을 때 발생할 수 있습니다.
+        if (result == -1) {
+            // 잘못된 UTF-8 형식
+            return 0;
+        }
+
+        // 한글, 영어 알파벳, 숫자 여부 확인
+        if ((wchar >= 0xAC00 && wchar <= 0xD7A3) || // 한글확인
+            (wchar >= L'A' && wchar <= L'Z') || (wchar >= L'a' && wchar <= L'z') || // 영어확인
+            (wchar >= L'0' && wchar <= L'9')) { // 숫자확인
+            return 1; // 한글, 영어 알파벳, 숫자 중 하나라도 있으면 1 반환
+        }
+
+        str += result;
+    }
+
+    return 0; // 모든 문자가 한글, 영어 알파벳, 숫자가 아닐 경우
 }
 
 //프로그램 시작
@@ -48,43 +115,4 @@ int main() {
     Print_Inventory_Management(Y_STORE);
 
     return 0;
-}
-
-// 한글 여부를 확인하는 함수 
-int isKorean(unsigned int codepoint) {
-    //유니코드 한글 코드표 참조
-    //https://namu.wiki/w/현대%20한글의%20모든%20글자/유니코드
-    return (codepoint >= 0xAC00 && codepoint <= 0xD7A3);
-}
-
-#include <stdio.h>
-#include <string.h>
-
-// 한글 여부를 확인하는 함수
-int isKorean(const char* str) {
-    unsigned char firstByte = (unsigned char)str[0];
-
-    // 한글은 UTF-8에서 3바이트로 인코딩되므로 첫 번째 바이트의 패턴을 확인
-    if ((firstByte & 0xE0) == 0xE0) {
-        // 두 번째 바이트의 패턴도 확인
-        if ((str[1] & 0xC0) == 0x80 && (str[2] & 0xC0) == 0x80) {
-            return 1;  // UTF-8로 인코딩된 한글
-        }
-    }
-
-    return 0;  // 한글이 아닌 경우
-}
-
-// 문자열에 포함된 모든 문자가 한글인지 확인하는 함수
-int allCharactersKorean(const char* str) {
-    while (*str) {
-        if (!isKorean(str)) {
-            return 0;  // 한 글자라도 한글이 아니면 0을 반환
-        }
-
-        // 다음 문자로 이동
-        str += ((*str & 0xE0) == 0xE0) ? 3 : 1;
-    }
-
-    return 1;  // 문자열의 모든 문자가 한글이면 1을 반환
 }
